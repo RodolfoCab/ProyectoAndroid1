188837279723ff4bdbb94ea14c0036940b40c1d4
Cab Martinez Rodolfo <recm_95@hotmail.com>
2016-05-19 10:03:57 -0500

    Versión con demostración de manejo de eventos del proyecto GeoApp

--- app/src/main/java/com/rodolfocab/android/geoapp/GeoActivity.java
+++ app/src/main/java/com/rodolfocab/android/geoapp/GeoActivity.java
@@ -7,13 +7,19 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class GeoActivity extends AppCompatActivity {
-
+    private Button mBotonCierto,mBotonFalso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo);
         mBotonCierto = (Button) findViewById(R.id.boton_cierto);
         mBotonFalso = (Button) findViewById(R.id.boton_falso);
+    }
+    public void True(View puto){
+        Toast.makeText(GeoActivity.this,R.string.texto_correcto,Toast.LENGTH_SHORT).show();
+    }
+    public void False(View vv){
+        Toast.makeText(GeoActivity.this,R.string.texto_incorrecto,Toast.LENGTH_SHORT).show();
     }
 }
 
